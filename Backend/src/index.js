d73aecf8d09bc3da377cd35de40296d270b1fd3c
LeonardoJGG <leonardoguevara873@gmail.com>
2023-04-10 03:06:33 -0400

    budget

---
+++ Backend/src/index.js
@@ -0,0 +1,25 @@
+import { config } from 'dotenv'
+import app from './api/api.js';
+import { sequelize } from './db/db.js';
+
+
+async function main() {
+
+    config();
+    const PORT = process.env.PORT;
+
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+
+        await sequelize.sync()
+
+        app.listen(PORT);
+        console.log(`Server is listening on port ${PORT}`);
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+
+}
+
+main();
